901a4aef72ca79303b59fedc6028f9787fdf9c09
Ahmadreza Badihi <badihi@atighgasht.ir>
2018-11-22 23:52:20 +0330

    Add project files.

---
+++ DBSecProject/Form1.cs
@@ -0,0 +1,44 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBSecProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                conn.Open();
+
+                // Insert some data
+                using (var cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "INSERT INTO data (some_field) VALUES (@p)";
+                    cmd.Parameters.AddWithValue("p", "Hello world");
+                    cmd.ExecuteNonQuery();
+                }
+
+                // Retrieve all rows
+                using (var cmd = new NpgsqlCommand("SELECT some_field FROM data", conn))
+                using (var reader = cmd.ExecuteReader())
+                    while (reader.Read())
+                        Console.WriteLine(reader.GetString(0));
+            }
+        }
+    }
+}
