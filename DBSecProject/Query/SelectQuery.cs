3177fe75b026a6fd868b2f43c1afb42b22ba2c16
Ahmadreza Badihi <badihi@atighgasht.ir>
2018-11-23 20:32:11 +0330

    SELECT SUM added

--- DBSecProject/Query/SelectQuery.cs
+++ DBSecProject/Query/SelectQuery.cs
@@ -13,21 +13,23 @@ namespace DBSecProject
     {
         public List<string> Fields { get; private set; }
         public string Conditions { get; set; }
+        public string Function { get; set; }
 
         public SelectQuery(string query)
         {
-            var match = Regex.Match(query, @"select\s+(?:((?:[A-Za-z_][A-Za-z_0-9]*|\*))\s*,\s*)*([A-Za-z_][A-Za-z_0-9]*|\*)\s+from\s+([A-Za-z_][A-Za-z_0-9]*)(\s+where\s+(.+))?", RegexOptions.IgnoreCase);
+            var match = Regex.Match(query, @"select\s+(sum)*\s*\(*\s*(?:((?:[A-Za-z_][A-Za-z_0-9]*|\*))\s*,\s*)*([A-Za-z_][A-Za-z_0-9]*|\*)\s*\)*\s+from\s+([A-Za-z_][A-Za-z_0-9]*)(\s+where\s+(.+))?", RegexOptions.IgnoreCase);
 
             if (!match.Success)
                 throw new WrongQueryException();
 
-            Fields = match.Groups[1].Captures
+            Function = match.Groups[1].Value;
+            Fields = match.Groups[2].Captures
                 .Cast<Capture>()
                 .Select(capture => capture.Value)
                 .ToList();
-            Fields.Add(match.Groups[2].Value);
-            TableName = match.Groups[3].Value;
-            Conditions = match.Groups.Count >= 4 ? match.Groups[4].Value : null;
+            Fields.Add(match.Groups[3].Value);
+            TableName = match.Groups[4].Value;
+            Conditions = match.Groups.Count >= 5 ? match.Groups[5].Value : null;
         }
 
         public override DataTable _Execute(NpgsqlConnection connection, SecurityLevel RSL, SecurityLevel RIL, int subjectId)
@@ -50,7 +52,21 @@ namespace DBSecProject
             var cmd = new NpgsqlCommand(query.ToString(), connection);
             using (var reader = cmd.ExecuteReader())
             {
-                return Read(reader, RSL, RIL);
+                var result = Read(reader, RSL, RIL);
+                if (Function.ToLower() == "sum")
+                {
+                    // Summation
+                    if (Fields.Count != 1)
+                        throw new Exception("Invalid column count for aggergation");
+                    var newDataTable = new DataTable();
+                    newDataTable.Columns.Add("Sum");
+                    var newRow = newDataTable.NewRow();
+                    newRow["Sum"] = result.Rows.Cast<DataRow>().Sum(row => Convert.ToInt32(row.ItemArray[0]));
+                    newDataTable.Rows.Add(newRow);
+                    return newDataTable;
+                }
+
+                return result;
             }
         }
     }
